c96015e96afe43216befd09c5ad055b24817c385
Nikulkumar Popatbhai Kukadiya <nk865270@dal.ca>
2024-08-10 20:57:18 -0300

    - D3 Read me file updated.

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -7,20 +7,4 @@ router.post('/register', register);
 router.post('/login', login);
 router.post('/reset-password', resetPassword);
 
-
-//
-// router.get('/confirm/:token', async (req, res) => {
-//     try {
-//       const { token } = req.params;
-//       const { email } = jwt.verify(token, config.secret);
-//
-//       await db.query('UPDATE Users SET is_active = true WHERE email = ?', [email]);
-//
-//       res.status(200).send('Email confirmed. You can now login.');
-//     } catch (error) {
-//       console.error(error);
-//       res.status(400).send('Invalid or expired token.');
-//     }
-//   });
-
 module.exports = router;
